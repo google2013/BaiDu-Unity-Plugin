5e8eb73b4d17c14ff377a94a522da75e53cc051a
unity-plugins <tanghuan198907@163.com>
2016-11-23 14:11:58 +0800

    update baidu sdk fix ios app store reject problem

--- Assets/Plugins/iOS/BaiduMobAdSDK.framework/Headers/BaiduMobAdSetting.h
+++ Assets/Plugins/iOS/BaiduMobAdSDK.framework/Headers/BaiduMobAdSetting.h
@@ -7,9 +7,12 @@
 #import "BaiduMobAdCommonConfig.h"
 
 @interface BaiduMobAdSetting : NSObject
+@property BOOL supportHttps;
 /**
  *  设置Landpage页面导航栏颜色
  */
 + (void)setLpStyle:(BaiduMobAdLpStyle)style;
++ (BaiduMobAdSetting *)sharedInstance;
+
 @end
 
